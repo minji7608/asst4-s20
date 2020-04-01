1cec9759210570d24c5b32346b26b1db75439804
Minji Kim <minjik4@andrew.cmu.edu>
2020-04-01 04:31:36 -0400

    implements new partition algo

--- code/partition.c
+++ code/partition.c
@@ -19,7 +19,7 @@
   You should feel free to try out different partitioning schemes.
 */
 
-compare(const void *s1, const void *s2) {
+int compare(const void *s1, const void *s2) {
   region_t *r1 = (region_t *)s1;
   region_t *r2 = (region_t *)s2;
 
@@ -32,78 +32,78 @@ compare(const void *s1, const void *s2) {
 }
 
 
-float stdev(region_t data[], int len, bool by_node) {
-    int sum = 0;
-    float mean;
-    float SD = 0.0;
-    int i;
+// float stdev(region_t data[], int len, bool by_node) {
+//     int sum = 0;
+//     float mean;
+//     float SD = 0.0;
+//     int i;
 
-    // stdev of node counts
-    if (by_node) {
-      for (i = 0; i < len; ++i) {
-        sum += data[i].node_count;
-      }
+//     // stdev of node counts
+//     if (by_node) {
+//       for (i = 0; i < len; ++i) {
+//         sum += data[i].node_count;
+//       }
     
-      mean = (float)(sum / len);
+//       mean = (float)(sum / len);
       
-      for (i = 0; i < len; ++i)
-          SD += pow(data[i].node_count - mean, 2);
+//       for (i = 0; i < len; ++i)
+//           SD += pow(data[i].node_count - mean, 2);
           
-      return sqrt(SD / len);
-    }
+//       return sqrt(SD / len);
+//     }
     
-    // stdev of edge counts
-    else {
-      for (i = 0; i < len; ++i) {
-        sum += data[i].edge_count;
-      }
+//     // stdev of edge counts
+//     else {
+//       for (i = 0; i < len; ++i) {
+//         sum += data[i].edge_count;
+//       }
     
-      mean = (float)(sum / len);
+//       mean = (float)(sum / len);
       
-      for (i = 0; i < len; ++i)
-          SD += pow(data[i].edge_count - mean, 2);
+//       for (i = 0; i < len; ++i)
+//           SD += pow(data[i].edge_count - mean, 2);
           
-      return sqrt(SD / len);
-    }
+//       return sqrt(SD / len);
+//     }
 
-}
+// }
 
-void assign_zones(region_t *region_list, int nregion, int nzone) {
-    // TODO:  This partitioner is very naive.  You can do better!
+// void assign_zones(region_t *region_list, int nregion, int nzone) {
+//     // TODO:  This partitioner is very naive.  You can do better!
 
-    qsort(region_list, nregion, sizeof(region_t), compare);
+//     qsort(region_list, nregion, sizeof(region_t), compare);
 
 
-    double weights[nregion];
-    int zones[nzone];
-    int rid, zid;
+//     double weights[nregion];
+//     int zones[nzone];
+//     int rid, zid;
   
-    float node_stdev = stdev(region_list, nregion, true);
-    float edge_stdev = stdev(region_list, nregion, false);
+//     float node_stdev = stdev(region_list, nregion, true);
+//     float edge_stdev = stdev(region_list, nregion, false);
     
-    if (node_stdev > edge_stdev) {
-      for (rid=0; rid<nregion; rid++) {
-        weights[rid] = (double)(region_list[rid].node_count);
-      }
-    }
+//     if (node_stdev > edge_stdev) {
+//       for (rid=0; rid<nregion; rid++) {
+//         weights[rid] = (double)(region_list[rid].node_count);
+//       }
+//     }
 
-    else {
-      for (rid=0; rid<nregion; rid++) {
-        weights[rid] = (double)(region_list[rid].edge_count);
-      }
-    }
+//     else {
+//       for (rid=0; rid<nregion; rid++) {
+//         weights[rid] = (double)(region_list[rid].edge_count);
+//       }
+//     }
 
-    find_partition(nregion, nzone, weights, zones);
+//     find_partition(nregion, nzone, weights, zones);
     
-    int curr_rid = 0;
-    for (zid=0; zid<nzone; zid++) {
-      int end_rid = curr_rid + zones[zid];
-      while (curr_rid < end_rid) {
-        region_list[curr_rid].zone_id = zid;
-        curr_rid++;
-      }
-    }
-}
+//     int curr_rid = 0;
+//     for (zid=0; zid<nzone; zid++) {
+//       int end_rid = curr_rid + zones[zid];
+//       while (curr_rid < end_rid) {
+//         region_list[curr_rid].zone_id = zid;
+//         curr_rid++;
+//       }
+//     }
+// }
 
 
 // int compare(const void *s1, const void *s2) {
@@ -118,72 +118,89 @@ void assign_zones(region_t *region_list, int nregion, int nzone) {
 //   else return 0;
 // }
 
-// float stdev(region_t data[], int len) {
-//   int sum = 0;
-//   float mean;
-//   float SD = 0.0;
-//   int i;
+float stdev_double(double data[], int len) {
+  double sum = 0;
+  float mean;
+  float SD = 0.0;
+  int i;
 
-//   // stdev of node counts
-//   for (i = 0; i < len; ++i) {
-//     sum += data[i].node_count;
-//     mean = (float)(sum / len);
+  // stdev of node counts
+  for (i = 0; i < len; ++i) {
+    sum += data[i];
+    mean = (float)(sum / len);
     
-//     for (i = 0; i < len; ++i)
-//       SD += pow(data[i].node_count - mean, 2);
-//   } 
+    for (i = 0; i < len; ++i)
+      SD += pow(data[i] - mean, 2);
+  } 
     
-//   return sqrt(SD / len);
-// }
+  return sqrt(SD / len);
+}
 
-// void assign_zones(region_t *region_list, int nregion, int nzone) {
-//     // TODO:  This partitioner is very naive.  You can do better!
+float stdev_int(int data[], int len) {
+  int sum = 0;
+  float mean;
+  float SD = 0.0;
+  int i;
 
-//     // qsort(region_list, nregion, sizeof(region_t), compare);
-
-
-//     double weights[nregion];
-//     double tempweights[nzone];
-//     int zones[nzone];
-//     int rid, zid;
-
-//     for (rid=0; rid<nregion; rid++) {
-//       int firstval = region_list[rid].edge_count;
-//       int secondval = region_list[rid].node_count;
-//       int finalval = firstval + secondval;
-//       weights[rid] = (double)(finalval);
-//     }
-
-
-//     // //interval partioning
-//     // int intervalnum = nregion/nzone;
-//     // int rid = 0;
-//     // tempweights[rid] = region_list[rid].node_count;
-//     // for(zid = 0; zid < nzone; zid++){
-//     //     if(rid % intervalnum != 0){
-//     //       tempweights[zid] = region_list[rid].node_count;
-//     //       rid++;
-//     //     }
-//     // }
-
-//     find_partition(nregion, nzone, weights, zones);
-
-
-//     int interval_stdev = stdev(zones, nzone);
-//     int greedy_stdev = stdev(tempweights, nzone);
-
-
-//     if(greedy_stdev < interval_stdev){
-
-      
+  // stdev of node counts
+  for (i = 0; i < len; ++i) {
+    sum += data[i];
+    mean = (float)(sum / len);
     
-//     int curr_rid = 0;
-//     for (zid=0; zid<nzone; zid++) {
-//       int end_rid = curr_rid + zones[zid];
-//       // fpri ntf(stderr, "%d\n", end_rid);
-//       while (curr_rid < end_rid) {
-//         region_list[curr_rid].zone_id = zid;
-//         curr_rid++;
-//       }
-//     }
-// }
+    for (i = 0; i < len; ++i)
+      SD += pow(data[i] - mean, 2);
+  } 
+    
+  return sqrt(SD / len);
+}
+
+
+
+
+    // TODO:  This partitioner is very naive.  You can do better!
+
+    // qsort(region_list, nregion, sizeof(region_t), compare);
+
+void assign_zones(region_t *region_list, int nregion, int nzone) {
+
+    double tempweights[nzone];
+    double weights[nregion];
+    int zones[nzone];
+    int rid, zid;
+
+    for (rid=0; rid<nregion; rid++) {
+      int firstval = region_list[rid].edge_count;
+      int secondval = region_list[rid].node_count;
+      int finalval = firstval + secondval;
+      weights[rid] = (double)(finalval);
+    }
+
+
+    //interval partioning
+    int intervalnum = nregion/nzone;
+    for (zid = 0; zid < nzone; zid++){
+        for(rid = intervalnum*zid; rid < intervalnum*(zid+1); rid++){
+          tempweights[zid] += (double)region_list[rid].node_count;
+          region_list[rid].zone_id = zid;
+        }
+    }
+
+    find_partition(nregion, nzone, weights, zones);
+
+
+    int interval_stdev = stdev_double(tempweights, nzone);
+    int greedy_stdev = stdev_int(zones, nzone);
+
+
+    if(greedy_stdev < interval_stdev){
+        int curr_rid = 0;
+        for (zid=0; zid<nzone; zid++) {
+          int end_rid = curr_rid + zones[zid];
+      // fpri ntf(stderr, "%d\n", end_rid);
+          while (curr_rid < end_rid) {
+          region_list[curr_rid].zone_id = zid;
+          curr_rid++;
+        }
+      }
+    }
+}
